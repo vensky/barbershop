71b29fbb6964908b29e66c13afab7bac13bd96bd
vensky <ivan.fedotovskikh@gmail.com>
2021-01-05 17:59:24 +0500

    Исправляет js модальных окон

--- src/js/modals_new.js
+++ src/js/modals_new.js
@@ -5,16 +5,17 @@ document.addEventListener('DOMContentLoaded', function() {
     let modalCloseButtons = document.querySelectorAll('.js-modalClose');
     let overlay = document.querySelector('.overlay');
 
-    console.log(modalOpenButtons)
-
     modalOpenButtons.forEach(function(item) {
         item.addEventListener('click', function(e) {
             e.preventDefault();
             let modalId = this.dataset.modal;
             let modalElem = document.querySelector(`#${modalId}`);
-            let modalActive = document.querySelector('.modal');
+            let modalActive = document.querySelectorAll('.modal');
 
-            modalActive.classList.remove('modal--active');
+
+            modalActive.forEach(function(item) {
+                item.classList.remove('modal--active');
+            });
             modalElem.classList.add('modal--active');
             overlay.classList.add('overlay--active');
 
