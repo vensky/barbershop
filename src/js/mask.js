ac9a7e6b202e15bd07206972ff084d60e13774e6
vensky <ivan.fedotovskikh@gmail.com>
2021-01-19 14:00:17 +0500

    Добавляет маски в форму

---
+++ src/js/mask.js
@@ -0,0 +1,7 @@
+let inputDate = document.querySelector('.js-dateMask');
+let inputTime = document.querySelector('.js-timeMask');
+let inputTel = document.querySelector('.js-telMask');
+
+let dateMask = IMask(inputDate, { mask: Date });
+let timeMask = IMask(inputTime, { mask: '00:00' });
+let telMask = IMask(inputTel, { mask: '(000) 000-00-00' });
